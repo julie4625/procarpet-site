61956d63e008d92095e63d5e9e54b788c4cc6807
julie4625 <bradafc24@hotmail.com>
2025-05-28 22:28:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.subscribe-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert("Thank you for subscribing! 🎉 We’ll let you know when the app goes live.");
+});
